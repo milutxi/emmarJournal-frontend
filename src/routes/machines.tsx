74f1f84bf12b31f82a8a11685e3a2e8b584bae5b
milutxi <aliaga_sheila@hotmail.com>
2026-04-29 18:48:56 +0200

    comment the button to create the machine for deploy without troubles

--- src/routes/machines.tsx
+++ src/routes/machines.tsx
@@ -1,58 +1,58 @@
-import CreateMachine from '../components/CreateMachine/createMachine';
-import ListMachines from '../components/Machines/listMachines';
-import styles from './machines.module.scss'
-import { Link } from 'react-router-dom';
-import { Machine } from '../types';
-import { useEffect, useState } from 'react';
+import CreateMachine from "../components/CreateMachine/createMachine";
+import ListMachines from "../components/Machines/listMachines";
+import styles from "./machines.module.scss";
+import { Link } from "react-router-dom";
+import { Machine } from "../types";
+import { useEffect, useState } from "react";
 const Machines = () => {
-    const [machines, setMachines] = useState<Machine[]>([]);
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(import.meta.env.VITE_BACKEND_URL + '/machine', {
-                    headers: {
-                        'Accepts': 'application/json'
-                    }
-                });
+  const [machines, setMachines] = useState<Machine[]>([]);
 
-                const data = await response.json();
-                
-                setMachines(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_BACKEND_URL + "/machine",
+          {
+            headers: {
+              Accepts: "application/json",
+            },
+          },
+        );
 
-            } catch (error) {
-                console.error("Error fetching machines: ", error);
-            }
-        };
-        fetchData();
-    })
+        const data = await response.json();
 
-    const addMachineClick = () => {
-        <CreateMachine />
-    }
+        setMachines(data);
+      } catch (error) {
+        console.error("Error fetching machines: ", error);
+      }
+    };
+    fetchData();
+  });
 
-    return (
-        <div className={styles.machinesStyle}>
-            <h1 className={styles["machinesStyle__title"]}>MASKINER</h1>
-            <div className={styles["machinesStyle__add"]} >
-                    {/* // on page load show list, add button onclick to show form */}
-                    <Link to ="/app/addMachine">
+  // const addMachineClick = () => {
+  //     <CreateMachine />
+  // }
+
+  return (
+    <div className={styles.machinesStyle}>
+      <h1 className={styles["machinesStyle__title"]}>MASKINER</h1>
+      <div className={styles["machinesStyle__add"]}>
+        {/* // on page load show list, add button onclick to show form */}
+        {/* <Link to ="/app/addMachine">
                         <button
                             className={styles["machinesStyle__button"]}
                             onClick={addMachineClick}>
                             CLICK HERE TO ADD A NEW MACHINE
                         </button> 
-                    </Link>
-            </div>
-            <div>
-                {machines.map(machine =>
-                    <ListMachines
-                        key={machine._id}
-                        machine={machine} />
-                )}
-            </div>
-        </div>
-    )
-}
+                    </Link> */}
+      </div>
+      <div>
+        {machines.map((machine) => (
+          <ListMachines key={machine._id} machine={machine} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Machines;
