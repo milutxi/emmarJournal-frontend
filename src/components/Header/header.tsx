41282e9c6af5bca8736d2900b6a1f488ef021f26
milutxi <aliaga_sheila@hotmail.com>
2026-05-13 10:53:51 +0200

    update navigation with active menu and style

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,30 +1,62 @@
-import { Link } from 'react-router-dom';
-import styles from './header.module.scss'
+import { Link, NavLink } from "react-router-dom";
+import styles from "./header.module.scss";
 
 const Header = () => {
-    return(
-        <div className={styles.header}>
-            <div>
-                <Link to="/">
-                <img className={styles["header__logo"]} src="https://assets-global.website-files.com/6394edc1cd797257200998bc/63963420883c851c66e77a08_emmar%20beauty%20logo.svg" alt="emmar logo"></img>
-                </Link>
-            </div>
-            <div className={styles["header__navigation"]}>
-                <Link to="/app/journal" className={styles["header__enlace"]}>
-                    <button className={styles["header__button"]}>JOURNAL</button>
-                </Link>
-                <Link to="/app/clients" className={styles["header__enlace"]}>
-                    <button className={styles["header__button"]}>KUNDER</button>
-                </Link>
-                <Link to="/app/treatments" className={styles["header__enlace"]}>
-                    <button className={styles["header__button"]}>BEHANDLINGAR</button>
-                </Link>
-                <Link to="/app/machines" className={styles["header__enlace"]}>
-                    <button className={styles["header__button"]}>MASKINER</button>
-                </Link>             
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.header}>
+      <div>
+        <Link to="/">
+          <img
+            className={styles["header__logo"]}
+            src="https://assets-global.website-files.com/6394edc1cd797257200998bc/63963420883c851c66e77a08_emmar%20beauty%20logo.svg"
+            alt="emmar logo"
+          ></img>
+        </Link>
+      </div>
+      <div className={styles["header__navigation"]}>
+        <NavLink
+          to="/app/journal"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles["header__button"]} ${styles["header__buttonActive"]}`
+              : styles["header__button"]
+          }
+        >
+          JOURNAL
+        </NavLink>
+        <NavLink
+          to="/app/clients"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles["header__button"]} ${styles["header__buttonActive"]}`
+              : styles["header__button"]
+          }
+        >
+          KUNDER
+        </NavLink>
+        <NavLink
+          to="/app/treatments"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles["header__button"]} ${styles["header__buttonActive"]}`
+              : styles["header__button"]
+          }
+        >
+          BEHANDLINGAR
+        </NavLink>
+        <NavLink
+          to="/app/machines"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles["header__button"]} ${styles["header__buttonActive"]}`
+              : styles["header__button"]
+          }
+        >
+          MASKINER
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
