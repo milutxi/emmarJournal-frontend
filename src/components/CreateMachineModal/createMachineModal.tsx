7eddd496e850e4986ece3e9fc2c05aa3612152d3
milutxi <aliaga_sheila@hotmail.com>
2026-05-21 11:11:06 +0200

    create the first step basic information for new machine inside the parent

--- src/components/CreateMachineModal/createMachineModal.tsx
+++ src/components/CreateMachineModal/createMachineModal.tsx
@@ -7,7 +7,14 @@ type Props = {
 
 const CreateMachineModal = ({ onClose }: Props) => {
   const [step, setStep] = useState(1);
-  
+  const [formData, setFormData] = useState({
+    mName: "",
+    mModelNumber: "",
+    mSerialNumber: "",
+    mManufactureCompany: "",
+    mManufactureYear: "",
+    mDescription: "",
+  });
 
   return (
     <div className={styles.overlay}>
@@ -18,7 +25,105 @@ const CreateMachineModal = ({ onClose }: Props) => {
         <h1>Ny maskin</h1>
 
         <div className={styles.stepContent}>
-          {step === 1 && <h2>Grundinformation</h2>}
+          {step === 1 && (
+            <div className={styles.formSection}>
+              <h2>Grundinformation</h2>
+              <div className={styles.rowOne}>
+                <div className={styles.inputGroup}>
+                  <label>Namn</label>
+
+                  <input
+                    type="text"
+                    value={formData.mName}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        mName: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className={styles.inputGroup}>
+                  <label>Modellnummer</label>
+
+                  <input
+                    type="text"
+                    value={formData.mModelNumber}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        mModelNumber: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className={styles.inputGroup}>
+                  <label>Serienummer</label>
+
+                  <input
+                    type="text"
+                    value={formData.mSerialNumber}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        mSerialNumber: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+
+              <div className={styles.rowTwo}>
+                <div className={styles.inputGroup}>
+                  <label>Tillverkare</label>
+
+                  <input
+                    type="text"
+                    value={formData.mManufactureCompany}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        mManufactureCompany: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+
+                <div className={styles.inputGroup}>
+                  <label>Tillverkningsår</label>
+
+                  <input
+                    type="date"
+                    value={formData.mManufactureYear}
+                    onChange={(e) =>
+                      setFormData({
+                        ...formData,
+                        mManufactureYear: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+
+              <div className={styles.inputGroup}>
+                <label>Beskrivning</label>
+
+                <textarea
+                  rows={5}
+                  value={formData.mDescription}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      mDescription: e.target.value,
+                    })
+                  }
+                />
+              </div>
+            </div>
+          )}
+
           {step === 2 && <h2>Anskaffning</h2>}
           {step === 3 && <h2>Service</h2>}
           {step === 4 && <h2>Kommentarer</h2>}
@@ -27,8 +132,8 @@ const CreateMachineModal = ({ onClose }: Props) => {
         <div className={styles.actions}>
           {step > 1 ? (
             <button onClick={() => setStep(step - 1)}>Tillbaka</button>
-          ): (
-          <div/>
+          ) : (
+            <div />
           )}
 
           {step < 4 ? (
