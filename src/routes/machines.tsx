efd336e72d02ee9d98ccf22e2643407ce185608b
milutxi <aliaga_sheila@hotmail.com>
2026-06-01 17:03:59 +0200

    style button ny machine and page setting

--- src/routes/machines.tsx
+++ src/routes/machines.tsx
@@ -3,100 +3,84 @@
 import ListMachines from "../components/Machines/listMachines";
 import styles from "./machines.module.scss";
 //import { Link } from "react-router-dom";
- import { Machine } from "../types";
+import { Machine } from "../types";
 import { useEffect, useState } from "react";
 import CreateMachineModal from "../components/CreateMachineModal/createMachineModal";
 const Machines = () => {
-    const [machines, setMachines] = useState<Machine[]>([]);
-    const [showCreateModal, setShowCreateModal] = useState(false);
+  const [machines, setMachines] = useState<Machine[]>([]);
+  const [showCreateModal, setShowCreateModal] = useState(false);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            import.meta.env.VITE_BACKEND_URL + "/machine",
-            {
-              headers: {
-                Accepts: "application/json",
-              },
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_BACKEND_URL + "/machine",
+          {
+            headers: {
+              Accepts: "application/json",
             },
-          );
+          },
+        );
 
-          const data = await response.json();
+        const data = await response.json();
 
-          setMachines(data);
-        } catch (error) {
-          console.error("Error fetching machines: ", error);
-        }
-      };
-      fetchData();
-    });
-
-    // const addMachineClick = () => {
-    //     <CreateMachine />
-    // }
-
-   const deleteMachine = async (id: string) => {
-  const confirmed = window.confirm(
-    "Are you sure you want to delete this machine?"
-  );
-
-  if (!confirmed) return;
-
-  try {
-    await fetch(
-      import.meta.env.VITE_BACKEND_URL + "/machine/" + id,
-      {
-        method: "DELETE",
+        setMachines(data);
+      } catch (error) {
+        console.error("Error fetching machines: ", error);
       }
+    };
+    fetchData();
+  });
+
+  // const addMachineClick = () => {
+  //     <CreateMachine />
+  // }
+
+  const deleteMachine = async (id: string) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this machine?",
     );
 
-    setMachines(
-      machines.filter((machine) => machine._id !== id)
-    );
+    if (!confirmed) return;
 
-  } catch (error) {
-    console.error("Error deleting machine:", error);
-  }
-};
+    try {
+      await fetch(import.meta.env.VITE_BACKEND_URL + "/machine/" + id, {
+        method: "DELETE",
+      });
 
-
+      setMachines(machines.filter((machine) => machine._id !== id));
+    } catch (error) {
+      console.error("Error deleting machine:", error);
+    }
+  };
 
   return (
-   <div className={styles.machinesStyle}>
-  
-  <div className={styles["machinesStyle__top"]}>
-    <h1 className={styles["machinesStyle__title"]}>
-      MASKINER
-    </h1>
+    <div className={styles.machinesStyle}>
+      <div className={styles["machinesStyle__top"]}>
+        <h1 className={styles["machinesStyle__title"]}>MASKINER</h1>
+        <button
+          className={styles["machinesStyle__createButton"]}
+          onClick={() => setShowCreateModal(true)}
+        >
+          + Ny maskin
+        </button>
+      </div>
 
-    <button
-    className={styles["machinesPage__createButton"]}
-    onClick={() => setShowCreateModal(true)}
-  >
-    + Ny maskin
-  </button>
-  </div>
+      <div className={styles["machinesStyle__grid"]}>
+        {machines.map((machine) => (
+          <ListMachines
+            key={machine._id}
+            machine={machine}
+            deleteMachine={deleteMachine}
+            // onEdit={setEditingMachine}
+          />
+        ))}
+      </div>
 
-  <div className={styles["machinesStyle__grid"]}>
-    {machines.map((machine) => (
-      <ListMachines
-        key={machine._id}
-        machine={machine}
-        deleteMachine={deleteMachine}
-        // onEdit={setEditingMachine}
-      />
-    ))}
-  </div>
-
-  {showCreateModal && (
-    <CreateMachineModal
-      onClose ={()=> setShowCreateModal(false)}
-      />
-  )}
-
-</div>
- 
+      {showCreateModal && (
+        <CreateMachineModal onClose={() => setShowCreateModal(false)} />
+      )}
+    </div>
   );
 };
 
