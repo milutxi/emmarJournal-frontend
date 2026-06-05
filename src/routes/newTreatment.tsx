0185a4803461b71f09fbff1b4843634c549cb488
milutxi <aliaga_sheila@hotmail.com>
2026-06-05 10:55:36 +0200

    add loader to the new treatment file, linked with client and tested

--- src/routes/newTreatment.tsx
+++ src/routes/newTreatment.tsx
@@ -1,12 +1,34 @@
 import styles from "./oneClient.module.scss";
+
+import { LoaderFunctionArgs, useLoaderData } from "react-router";
+import { Client } from "../types";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const { id } = params;
+
+  const response = await fetch(
+    import.meta.env.VITE_BACKEND_URL + "/clients/" + id,
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    },
+  );
+
+  return response.json();
+};
+
 const NewTreatmen = () => {
+  const client = useLoaderData() as Client;
 
-
-  return(
+  return (
     <div className={styles.newTreatmentStyle}>
- <h1>New Treatment Session</h1>
+      <h1>New Treatment Session</h1>
+      <p>
+        {client.name} {client.lastName}
+      </p>
     </div>
-  )
+  );
 };
 
 export default NewTreatmen;
