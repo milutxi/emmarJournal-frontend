63b34b2b646614a86ee5e9a72dc938563c2a01cd
milutxi <aliaga_sheila@hotmail.com>
2026-09-16 09:25:39 +0200

    style in Machine cards is nicer

--- src/components/Machines/listMachines.tsx
+++ src/components/Machines/listMachines.tsx
@@ -1,49 +1,43 @@
 import { Machine } from "../../types";
 import styles from "./listMachines.module.scss";
-//import { RiDeleteBinLine } from "react-icons/ri";
-
 import { Link } from "react-router-dom";
 
 interface Props {
   machine: Machine;
-  // deleteMachine: (id: string) => void;
 }
 
-const ListMachines = ({ 
-  machine, 
-  // deleteMachine 
-}: Props) => {
+const ListMachines = ({ machine }: Props) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.card__header}>
-        <h3>{machine.mName}</h3>
-      </div>
+    <article className={styles.card}>
+      <Link to={`/app/machines/${machine._id}`} className={styles.card__link}>
+        <div className={styles.card__header}>
+          <h3>{machine.mName}</h3>
+        </div>
 
-      <Link to={`/app/machines/${machine._id}`} className={styles.card__body}>
-        <p>
-          <strong>Company:</strong> {machine.mManufactureCompany}
-        </p>
-        <p>
-          <strong>Model:</strong> {machine.mModelNumber}
-        </p>
-        <p>
-          <strong>Serial:</strong> {machine.mSerialNumber}
-        </p>
+        <div className={styles.card__body}>
+          <div className={styles.card__row}>
+            <span className={styles.card__label}>Tillverkare</span>
+            <span className={styles.card__value}>
+              {machine.mManufactureCompany || "-"}
+            </span>
+          </div>
+
+          <div className={styles.card__row}>
+            <span className={styles.card__label}>Modell</span>
+            <span className={styles.card__value}>
+              {machine.mModelNumber || "-"}
+            </span>
+          </div>
+
+          <div className={styles.card__row}>
+            <span className={styles.card__label}>Serienummer</span>
+            <span className={styles.card__value}>
+              {machine.mSerialNumber || "-"}
+            </span>
+          </div>
+        </div>
       </Link>
-
-      <div className={styles.card__actions}>
-        <section className={styles.card__edit}>
-          
-        </section>
-
-        {/* <section
-          className={styles.card__delete}
-          onClick={() => deleteMachine(machine._id)}
-        >
-          <RiDeleteBinLine />
-        </section> */}
-      </div>
-    </div>
+    </article>
   );
 };
 
