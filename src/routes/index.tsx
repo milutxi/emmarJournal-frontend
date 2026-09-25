dc649310adfbb36853ac5b8c0af2c35372bd71cc
milutxi <aliaga_sheila@hotmail.com>
2026-09-25 14:13:09 +0200

    make save/landing page menu better responsive and also in mobile form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,33 @@
-import { Link } from 'react-router-dom';
-import styles from './index.module.scss'; // Assuming you want to style it
+import { Link } from "react-router-dom";
+import styles from "./index.module.scss"; // Assuming you want to style it
 
 const Index = () => {
   return (
     <div className={styles.index}>
-        <div className={styles.header}>
-
-            <img className={styles["header__logo"]} src="https://assets-global.website-files.com/6394edc1cd797257200998bc/63963420883c851c66e77a08_emmar%20beauty%20logo.svg" alt="emmar logo"></img>
-        </div>
-    <div className={styles.indexContainer}>
-      <div className={styles.buttonContainer}>
-        <Link to="/app/journal" className={styles.link}>
-          <button className={styles.button}>Journal</button>
-        </Link>
-        <Link to="/app/clients" className={styles.link}>
-          <button className={styles.button}>Kunder</button>
-        </Link>
-        </div>
-        <br></br>
+      <header className={styles.header}>
+        <img
+          className={styles["header__logo"]}
+          src="https://assets-global.website-files.com/6394edc1cd797257200998bc/63963420883c851c66e77a08_emmar%20beauty%20logo.svg"
+          alt="emmar logo"
+        />
+      </header>
+      <main className={styles.indexContainer}>
         <div className={styles.buttonContainer}>
-        <Link to="/app/treatments" className={styles.link}>
-          <button className={styles.button}>Behandlingar</button>
-        </Link>
-        <Link to="/app/machines" className={styles.link}>
-          <button className={styles.button}>Maskiner</button>
-        </Link>
-      </div>
-    </div>
+          <Link to="/app/journal" className={styles.link}>
+            <span className={styles.button}>Journal</span>
+          </Link>
+          <Link to="/app/clients" className={styles.link}>
+            <span className={styles.button}>Kunder</span>
+          </Link>
+
+          <Link to="/app/treatments" className={styles.link}>
+            <span className={styles.button}>Behandlingar</span>
+          </Link>
+          <Link to="/app/machines" className={styles.link}>
+            <span className={styles.button}>Maskiner</span>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };
